1dbcd89b500924bcc6d711ca7c37b642b7bb7814
Jingyu Li <jingyul9@illinois.edu>
2022-11-07 15:17:53 -0600

    connect to db

--- order_system/backend/index.js
+++ order_system/backend/index.js
@@ -1,28 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
+
+const app = express();
 
 var db = mysql.createConnection({
     host: '35.224.10.239',
     user: 'root',
     password: 'Produce101',
     database:'test',
-})
+});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+
+app.get('/', (require, response) => {
+    response.send({'message': 'Hello World'});
+});
 
 app.get('/api/search', (require, response) => {
-    const foodName = require.body.foodName;
-    const sqlCommand = "SELECT DishId, DishName, Price, Description FROM Dishes WHERE DishName LIKE `%?%`";
-    db.query(sqlCommand, [foodName], (err, result) => {
+    const foodName = require.query.foodName;
+    console.log("Food Name:", foodName);
+    const sqlCommand = `SELECT DishId, DishName, Price, Description FROM Dishes WHERE DishName LIKE "%${foodName}%"`;
+    console.log("SQL:", sqlCommand);
+    db.query(sqlCommand, (err, result) => {
         response.send(result);
-    })
-})
+    });
+});
 
 app.listen(3002, () => {
-    console.log("running on port 3002");
-})
+    console.log("Server is running on port 3002");
+});
--- order_system/frontend/src/App.js
+++ order_system/frontend/src/App.js
@@ -1,73 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
-import Axios from "axios"
+import Axios from "axios";
 
-import { Input, Space, Table} from 'antd'
-import 'antd/dist/antd.css'
-
+import { Input, Space, Table} from 'antd';
+import "antd/dist/antd.css";
 // Should name the function starting with a capital letter!!
-
-const columns = [
-  {
-    title: 'DishId',
-    dataIndex: 'dishid',
-    key: 'dishid',
-  },
-  {
-    title: 'DishName',
-    dataIndex: 'dishname',
-    key: 'dishname',
-  },
-  {
-    title: 'Price',
-    dataIndex: 'price',
-    key: 'price',
-  },
-  {
-    title: 'Description',
-    dataIndex: 'description',
-    key: 'description',
-  },
-];
-
-const testData = [
-  {
-    key: '1',
-    dishid: '10000',
-    dishname: 'test food',
-    price: 100,
-    description: 'test'
-  },
-];
 
 
 const {Search} = Input;
 
 function App() {
   const [foodName, setFoodName] = useState('');
+  const [foodList, setFoodList] = useState([]);
 
-  const onSearch = () => {
-    Axios.post('http://localhost:3002/api/search', {
-      foodName: foodName
-    }).then(() => {
-
-    })
+  const onSearch = (value) => {
+    // console.log(value);
+    console.log("FoodName:", foodName);
+    Axios.get('http://localhost:3002/api/search', {
+      params: {
+        foodName: foodName
+      }
+    }).then((response) => {
+      // alert('success search');
+      console.log(response.data);
+      setFoodList(response.data.map(row => ({
+        dishid: row.DishId,
+        dishname: row.DishName,
+        price: row.Price,
+        description: row.Description
+      })));
+    });
   };
+
+  const columns = [
+    {
+      title: 'DishId',
+      dataIndex: 'dishid',
+      key: 'dishid',
+    },
+    {
+      title: 'DishName',
+      dataIndex: 'dishname',
+      key: 'dishname',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+    },
+  ];
+
+  // const testData = [
+  //   {
+  //     key: '1',
+  //     dishid: '10000',
+  //     dishname: 'test food',
+  //     price: 100,
+  //     description: 'test'
+  //   },
+  // ];
 
   return (
     <div className="App">
       <header className="App-header">
       <h1>Food Search</h1>
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      <Search placeholder='input food name' onSearch={onSearch} enterButton="Search" allowClear style={{ width: 304 }} size="large"></Search>
-      <p></p>
-      <Table columns={columns} dataSource={testData} />
-      <p></p>
-      <p></p>
+      <Search 
+        placeholder='input food name' 
+        enterButton="Search" 
+        allowClear 
+        style={{ width: 300, padding: 0, height: 80, margin: 0}} 
+        size="large"
+        onSearch={onSearch} 
+        onChange={(e) => setFoodName(e.target.value)}
+      />
+
+      <Table 
+        columns={columns} 
+        dataSource={foodList} 
+        style = {{width: 800, height: 500, padding: 0}}
+      />
       <a
         className="App-link"
         href="https://github.com/cs411-alawini/fa22-cs411-Q-team050-Produce101"
